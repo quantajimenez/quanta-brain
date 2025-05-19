54d013cd7fa9052467018ce362af60eb27a8b8eb
quantajimenez <dj.quanta.jjpro@gmail.com>
2025-05-19 15:38:32 -0400

    Create logger.py

---
+++ quanta/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+def setup_logger(name="quanta"):
+    logger = logging.getLogger(name)
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(name)s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
