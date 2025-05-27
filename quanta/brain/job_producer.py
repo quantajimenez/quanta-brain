9936e25f0198491dc9bed5d23e53745bbd694cad
quantajimenez <dj.quanta.jjpro@gmail.com>
2025-05-27 14:15:50 -0400

    Update job_producer.py

--- quanta/brain/job_producer.py
+++ quanta/brain/job_producer.py
@@ -8,10 +8,18 @@ import boto3
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
+REDIS_URL = os.environ.get("REDIS_URL")
+
+def send_heartbeat(worker_name):
+    try:
+        r = redis.from_url(REDIS_URL)
+        r.set(f"health_{worker_name}", time.time())
+    except Exception as e:
+        print(f"Heartbeat error for {worker_name}: {e}")
+
 S3_BUCKET = os.getenv("S3_HIST_BUCKET", "quanta-historical-marketdata")
 S3_PREFIX = os.getenv("S3_POLYGON_PREFIX", "polygon/")
 AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
-REDIS_URL = os.environ.get("REDIS_URL")
 REDIS_JOBS_KEY = os.getenv("REDIS_JOBS_KEY", "quanta_jobs")
 REDIS_SET_KEY = os.getenv("REDIS_SET_KEY", "quanta_jobs_submitted")
 
@@ -45,6 +53,7 @@ def mark_queued(job_id):
 def main():
     logging.info("Job Producer is running and connected to Redis...")
     while True:
+        send_heartbeat("job_producer")
         keys = get_all_s3_keys()
         logging.info(f"Found {len(keys)} files in S3 under {S3_PREFIX}")
         for key in keys:
